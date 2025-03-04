04560f1a0d0e2950620ef00ed8bdb6b5e5f5d54d
nwsluuw <93959251+nwsluuw@users.noreply.github.com>
2025-03-04 14:14:14 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const REGISTRY = 'https://registry.npmmirror.com';
+export const REGISTRY = 'http://192.168.1.223:7001';
 export const SYNC_REGISTRY = 'https://registry-direct.npmmirror.com';
